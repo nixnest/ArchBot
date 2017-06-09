048975f1f02df7a12b2afcab058141c7fb7ff3f9
Nhatz HK <nhatz.info@gmail.com>
2017-06-09 08:44:19 -0400

    Fixed replace for pasta commands A shallow copy of pasta[command] was needed. Although this is fixed now, I wouldn't be surprised if node breaks it due to the fact that it caches loaded modules. When/If that happens, move the content of sed.js directly into main.js or install one of thoe stupid and useless packages that claim to delete the cache.

---
+++ commands/sed.js
@@ -0,0 +1,24 @@
+function sedStr (str, arg) {
+    let str_ = str;
+    let arg_ = arg;
+    arg_ = arg_.split ('/');
+    if (typeof (arg_[1]) == 'undefined' ||
+        (arg_[0].length < 1 || arg_[1].length < 1)) {
+        console.log ("We'll skip that one: [Existent: ", arg_[0],
+            "\tReplacement: ", arg_[1], "]\n");
+    } else {
+        re = new RegExp (arg_[0], "gi");
+        str_ = str_.replace (re, arg_[1]);
+    }
+
+    return str_;
+}
+
+exports.run = function sed (str, arg_arr) {
+    let clean = str;
+    for (i in arg_arr) {
+        clean.embed.description = sedStr (clean.embed.description, arg_arr[i]);
+    }
+    return clean;
+}
+
