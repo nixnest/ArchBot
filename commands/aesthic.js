d92c326b2c86fdad6079a20f5778b550dc92c0dc
Tadeo Kondrak <tdeo@comprintor>
2017-06-28 20:33:48 -0600

    used a module, hahahaha

--- commands/aesthic.js
+++ commands/aesthic.js
@@ -1,9 +1,6 @@
+// const utils = require('../lib/utils.js')
+const fullwidth = require('fullwidth').default;
+
 exports.run = function (msg, args) {
-  var fullWidth = '';
-  for (var i=0; i < args.length; i++) {
-    var char = args[i].charCodeAt(0);
-    char += 0xFEE0;
-    fullWidth += String.fromCharCode(char);
-  }
-  msg.channel.send(fullWidth);
+  msg.channel.send(fullwidth(args.join('')));
 };
