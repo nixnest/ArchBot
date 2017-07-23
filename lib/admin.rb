24de8cd82271b00138f3430de2a807f034c837c0
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2017-07-23 12:06:34 -0300

    Fixed clear command to not consider its invocation

--- lib/admin.rb
+++ lib/admin.rb
@@ -4,7 +4,7 @@ module Admin
   command :clear do |event, *args|
     unless $userType != 'sudoer'
       event.channel.send_message("Invalid amount") && break if args[0].to_i > 100
-      toDelete = event.channel.history(args.empty? ? 100 : args[0].to_i)
+      toDelete = event.channel.history(args.empty? ? 100 : args[0].to_i+1)
       event.channel.delete_messages(toDelete)
     else
       event.channel.send_message("Permission denied") && break
